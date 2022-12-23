c775aaac677991ec99e677993c544b0b1f733d50
Your Name <you@example.com>
2022-12-23 14:15:53 +0530

    style(man 14):changed the font weight in cotact us page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { ContactComponent } from './contact/contact.component';
     AppComponent,
     NavbarComponent,
     ContentComponent,
-    AboutComponent
+    AboutComponent,
     ContactComponent
   ],
   imports: [
